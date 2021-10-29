9cfce9c2bc5e76b4a2fee88137c4d7058b688c67
albertobf <alberto_bernalf@yahoo.com.co>
2021-10-29 16:34:53 -0500

    tercer commit creo

--- consume/aplicacion/views.py
+++ consume/aplicacion/views.py
@@ -1,26 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, TemplateView, View
 import requests
+from django.http import FileResponse, Http404
 import json
 from django.http import HttpResponse, JsonResponse
+import io
+
 
 # Create your views here.
 
 def data1(request):
-    url = "http://localhost:8000/usuario/usuario/"
+    url = "http://localhost:8000/reportes/"
     #url = "http://jacgsaw.com/siath/app/war"
     datos =     {"url": "popeye@yahoo.com.co","username": "popeye", "email": "popeye@yahoo.com.co", "is_staff": "S"}
-    response = requests.post(url, data=datos)
+    respuesta = requests.get(url, data=datos)
 
-    if response.status_code == 200:
-        print(response.text)
-        resultado0 = response.text
-        
+    if respuesta.status_code == 200:
+        print("Esto recibo del REST")
+        print (respuesta.status_code)
+        buffer = io.BytesIO()
+        buffer.write(respuesta.content)
+
+        buffer.close()
+        return FileResponse(respuesta, filename='somename.pdf' , content_type='application/pdf')
 
     context = {}
-    # Pruebas
-    
+  
     return render(request, 'inicio4.html', context)
+
 
 
 
